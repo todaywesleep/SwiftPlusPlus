90c31dbdde94471b7f66f8a6f8c126fea5105b3c
Vladislav Erchik <vladislav.erchik@itrexgroup.com>
2022-11-16 12:34:42 +0100

    Initial commit

---
+++ SwiftPlusPlus/SwiftPlusPlus.h
@@ -0,0 +1,18 @@
+//
+//  SwiftPlusPlus.h
+//  SwiftPlusPlus
+//
+//  Created by Vladislav Erchik on 16.11.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftPlusPlus.
+FOUNDATION_EXPORT double SwiftPlusPlusVersionNumber;
+
+//! Project version string for SwiftPlusPlus.
+FOUNDATION_EXPORT const unsigned char SwiftPlusPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPlusPlus/PublicHeader.h>
+
+
